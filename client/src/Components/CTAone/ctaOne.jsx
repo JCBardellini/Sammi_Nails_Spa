0e3db7fbe655beffda2bf285b578cecf75144e5e
JCBardellini <115193170+JCBardellini@users.noreply.github.com>
2023-11-21 10:08:25 -0800

    added ctaOne and fix margin

---
+++ client/src/Components/CTAone/ctaOne.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./ctaOne.css";
+import { useNavigate } from "react-router-dom";
+
+const CtaOne = () => {
+  const navigate = useNavigate();
+  return (
+    <div id="ctaOne">
+      <h2 className="ctaTitle">Express Yourself with Unique Nails</h2>
+      <p>Book an appointment today and let your nails shine with creativity.</p>
+      <div className="callButtons">
+        <button className="contactUs" onClick={() => navigate("/contact")}>
+          Book
+        </button>
+        <button className="learnMore" onClick={() => navigate("/service")}>
+          Learn More
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CtaOne;
--- client/src/Pages/About/about.jsx
+++ client/src/Pages/About/about.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutContent from "../../Components/About/aboutContent";
 import Process from "../../Components/Process/process";
+import CtaOne from "../../Components/CTAone/ctaOne";
 
 const About = () => {
   return (
     <div>
       <AboutContent />
       <Process />
+      <CtaOne />
     </div>
   );
 };
